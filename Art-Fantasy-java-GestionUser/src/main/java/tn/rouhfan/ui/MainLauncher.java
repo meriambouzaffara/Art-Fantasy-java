f6bec770d984b1a6a26287bb1f2e7f6d778e22e7
meriambouzaffara <Meriam.Bouzaffara@esprit.tn>
2026-05-03 21:46:16 +0100

    Final integration and manual fixes

--- Art-Fantasy-java-GestionUser/src/main/java/tn/rouhfan/ui/MainLauncher.java
+++
@@ -1,8 +0,0 @@
-package tn.rouhfan.ui;
-
-public class MainLauncher {
-    public static void main(String[] args) {
-        // Cette classe sert de point d'entrée non-JavaFX pour éviter les problèmes de chargement des composants runtime
-        AppLauncher.main(args);
-    }
-}
